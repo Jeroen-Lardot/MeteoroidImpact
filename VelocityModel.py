dcd1ff68e385ae3430a79a06876f6a1cc436852f
Jeroen Lardot <jeroen.lardot@hotmail.com>
2021-04-07 15:49:15 +0200

    Updated Velocitymodel structure

--- VelocityModel.py
+++ VelocityModel.py
@@ -1,6 +1,22 @@
 class VelocityModel:
-    def __init__(self, data):
-        self.data = data
+    def __init__(self, masses):
+        self.masses = masses
+        self.velocities = self.__calculateVelocities()
 
-    def setVelocityModel(self):
-        return list(self.df["mass"])
+    def getVelocities(self):
+        return self.velocities
+
+    def __calculateVelocities(self):
+        particleVelocities = []
+        for mass in self.masses:
+            particleVelocities.append(self.__velocityModel(mass))
+        return particleVelocities
+
+    def __velocityModel(self, mass):
+        if(mass > 2):
+            return 1
+        if(1 < mass <= 2):
+            return 0
+        if(mass <= 1):
+            return -1
+        return None
