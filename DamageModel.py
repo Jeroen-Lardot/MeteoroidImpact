668af00eb792e5d1dc41b93e064659e83ffbb760
Jeroen-Lardot <jeroen.lardot@hotmail.com>
2021-04-13 13:58:34 +0200

    Added clearness and models in DamageModel.py

--- DamageModel.py
+++ DamageModel.py
@@ -4,11 +4,24 @@ class DamageModel:
         self.densities = densities
         self.velocities = velocities
 
-    def isPenetrated(self, sample, diameter, density, velocity):
-        if(sample < self.__criticalRadius(diameter, density, velocity)):
+    def isPenetrated(self, surfaceDiameter, particleDensity, particleVelocity):
+        if (surfaceDiameter < self.__criticalDiameter(surfaceDiameter, particleDensity, particleVelocity)):
             return True
         else:
             return False
 
-    def __criticalRadius(self, diameter, density, velocity):
-        return 0.65 * diameter ** (0.056) * density ** (0.52) * velocity ** (0.875)
+    # Returns the marginal diameter (micrometer) of the impacting particle at which point the
+    # surface diameter (micrometer) will be completely penetrated by a particle of density (g/cm3) and velocity (km/s)
+    def __criticalDiameter(self, surfaceDiameter, particleDensity, particleVelocity):
+        return (surfaceDiameter / (0.65*particleDensity ** (0.52) * particleVelocity ** (0.875)))**(1/1.056)
+
+    # integral A(m) f(m) dm, where A(m) is given by __areaDamagePerMass
+    def __areaDamageRate(self, areas, fluxes):
+        # TODO
+        return None
+
+    # Gives the damaged surface area by a particle of mass m and velocity V, given surface properties
+    def __areaDamagePerMass(self, mass, particleVelocity, surfaceDensity, surfaceDiameter):
+        # TODO
+        return None
+
